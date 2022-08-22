d80144455647d952a9e6673e07bbe97aff42ed80
ShulpinP <pavelshulpin6@gmail.com>
2022-08-22 15:02:46 +0300

    1

--- src/main/java/com/epam/m1/exceptions/ParseIntegers.java
+++ src/main/java/com/epam/m1/exceptions/ParseIntegers.java
@@ -15,15 +15,23 @@ public class ParseIntegers {
                     "JDK 17 has released on 14 September 2021 with 10 new features, 2 feature removals and 2 feature deprecations."
                             .split(" "));
 
-    public static void main(String[] args) {
-        Iterator<String> words = WORDS.iterator();
-        int sum = 0;
-        String justWords = "";
-        while (words.hasNext()) {
-            String next = words.next();
-            int number = Integer.parseInt(next);
-            // todo: complete it
-        }
+    public static void main(String[] args) throws  NumberFormatException{
+            Iterator<String> words = WORDS.iterator();
+            int sum = 0;
+            StringBuilder justWords = new StringBuilder();
+            while (words.hasNext()) {
+                        String next = words.next();
+                try {
+                        int number = Integer.parseInt(next);
+                        sum += number;
+                    }
+                    catch (NumberFormatException e1) {
+                        justWords.append(next).append(" ");
+                    }
+
+            }
+
+
         System.out.println("Sum is " + sum);
         System.out.println("Just words:" + justWords);
     }
